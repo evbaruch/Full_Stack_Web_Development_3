f276cd0a49cfff2e663fa7b38546e681c081684e
aviaMader <100083767+aviaMader@users.noreply.github.com>
2025-03-08 23:09:45 +0200

    Enhance network request simulation; add response drop probability and delay to improve realism

--- JS/network.js
+++ JS/network.js
@@ -1,12 +1,13 @@
 import Server from "../Server/server.js";
 
+const DROP_PROBABILITY = 0.2;
 const Network = {
   sendRequest(request, data) {
-    const delay = Math.random() * 2000 + 1000;
-    const dropProbability = Math.random();
+    const delayToServer = Math.random() * 500 + 1000; // at least 1 second up to 1.5 seconds
+    const dropProbabilityToServer = Math.random();
 
     setTimeout(() => {
-      if (dropProbability < 0.2) {
+      if (dropProbabilityToServer < DROP_PROBABILITY) {
         // 20% chance of dropping the request
         console.warn("FAJAX: Request lost in the network simulation.");
         request.readyState = 4;
@@ -22,12 +23,27 @@ const Network = {
       //request._triggerOnLoad();
 
       Server.handleRequest(request.method, request.url, data, (response) => {
-        request.status = response.status;
-        request.responseText = JSON.stringify(response.data);
-        request.readyState = 4;
-        request._triggerOnLoad();
+        const dropProbabilityToClient = Math.random();
+        const delayToClient = Math.random() * 500 + 1000; // at least 1 second up to 1.5 seconds
+        setTimeout(() => {
+          if (dropProbabilityToClient < DROP_PROBABILITY) {
+            // 20% chance of dropping the response
+            console.warn("FAJAX: Response lost in the network simulation.");
+            request.readyState = 4;
+            request.status = 504;
+            request.responseText = JSON.stringify({
+              message: "Response lost in the network simulation.",
+            });
+            request._triggerOnLoad();
+            return;
+          }
+          request.status = response.status;
+          request.responseText = JSON.stringify(response.data);
+          request.readyState = 4;
+          request._triggerOnLoad();
+        }, delayToClient);
       });
-    }, delay);
+    }, delayToServer);
   },
 };
 
